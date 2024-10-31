6e469e0bef2047b30dffbc591705b19980beb538
Domochip <33293764+Domochip@users.noreply.github.com>
2024-10-31 16:56:27 +0000

    Make esp32 works with Upgrade (#20)
    * Fix ESP32 Online Update
    
    * Adjust ESP32 URL for Online Update
    
    * change firmware name convention for ESP32
    
    * Enable ESP32 compilation by default
    
    * Update fw.html

--- src/base/Application.cpp
+++ src/base/Application.cpp
@@ -185,7 +185,11 @@ bool Application::updateFirmware(const char *version, String &retMsg, std::funct
   clientSecure.setInsecure();
 
   String fwUrl(F("https://github.com/" CUSTOM_APP_MANUFACTURER "/" CUSTOM_APP_MODEL "/releases/download/"));
+#ifdef ESP8266
   fwUrl = fwUrl + version + '/' + F(CUSTOM_APP_MODEL) + '.' + version + F(".bin");
+#else
+  fwUrl = fwUrl + version + '/' + F(CUSTOM_APP_MODEL) + F(".esp32") + '.' + version + F(".bin");
+#endif
 
   LOG_SERIAL_PRINTF_P(PSTR("Trying to Update from URL: %s\n"), fwUrl.c_str());
 
@@ -217,11 +221,7 @@ bool Application::updateFirmware(const char *version, String &retMsg, std::funct
   if (progressCallback)
     Update.onProgress(progressCallback);
 
-#ifdef ESP8266
   Update.begin(contentLength);
-#else
-  Update.begin();
-#endif
 
   // sometime the stream is not yet ready (no data available yet)
   // and writeStream start by a peek which then fail
