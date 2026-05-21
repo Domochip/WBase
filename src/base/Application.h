e1fef1a77d7dd9e04638aba1f8ef90b07e4ce296
DomoChip <domochip@hotmail.com>
2026-05-21 16:32:18 +0200

    Reorder Application signature and fillStatusJSON is public

--- src/base/Application.h
+++ src/base/Application.h
@@ -47,17 +47,6 @@ protected:
   static void fillLatestUpdateInfoJson(JsonVariant json, bool forWebPage = false);
   static bool updateFirmware(const char *version, String &retMsg, std::function<void(size_t, size_t)> progressCallback = nullptr);
 
-  // specialization required from the application
-  virtual void setConfigDefaultValues() = 0;
-  virtual bool parseConfigJSON(JsonVariant json, bool fromWebPage = false) = 0;
-  virtual void fillConfigJSON(JsonVariant json, bool forSaveFile = false) = 0;
-  virtual void fillStatusJSON(JsonVariant json) = 0;
-  virtual bool appInit(bool reInit = false) = 0;
-  virtual const PROGMEM char *getHTMLContent(WebPageForPlaceHolder wp) = 0;
-  virtual size_t getHTMLContentSize(WebPageForPlaceHolder wp) = 0;
-  virtual void appInitWebServer(WebServer &server) = 0;
-  virtual void appRun() = 0;
-
 public:
   Application(AppId appId);
 
@@ -67,6 +56,23 @@ public:
   void init(bool skipExistingConfig);
   void initWebServer(WebServer &server);
   void run();
+
+  // ----------
+  // specialization required from the application
+  // ----------
+
+protected:
+  virtual bool appInit(bool reInit = false) = 0;
+  virtual void appInitWebServer(WebServer &server) = 0;
+  virtual void appRun() = 0;
+  virtual void fillConfigJSON(JsonVariant json, bool forSaveFile = false) = 0;
+  virtual const PROGMEM char *getHTMLContent(WebPageForPlaceHolder wp) = 0;
+  virtual size_t getHTMLContentSize(WebPageForPlaceHolder wp) = 0;
+  virtual bool parseConfigJSON(JsonVariant json, bool fromWebPage = false) = 0;
+  virtual void setConfigDefaultValues() = 0;
+
+public:
+  virtual void fillStatusJSON(JsonVariant json) = 0;
 };
 
 #endif
